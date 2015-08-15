06f6679dac233f3ba8dcb265ef7e640fa05cd498
Ho Yan Leung <hy.leung@gmail.com>
2015-08-14 20:27:13 -0700

    Initial commit

---
+++ src/main/java/rx/testkit/RxTestKitSubscriber.java
@@ -0,0 +1,15 @@
+package rx.testkit;
+
+import rx.observers.TestSubscriber;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Date: 15-08-14
+ * Time: 7:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RxTestKitSubscriber<T> extends TestSubscriber<T> {
+
+}
