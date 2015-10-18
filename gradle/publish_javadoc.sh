24cd0682b925bcb9faca998393724ba44a72f603
Ho Yan Leung <hy.leung@gmail.com>
2015-10-18 14:39:39 -0700

    Javadoc publish script tweak

--- gradle/publish_javadoc.sh
+++ gradle/publish_javadoc.sh
@@ -3,12 +3,15 @@
 echo "Cloning gh-pages..."
 cd build
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hyleung/rx-testkit gh-pages 
-cp -r doc/javadoc gh-pages
+
 cd gh-pages
 git config --local user.email "travis@travis-ci.org"
 git config --local user.name "travis ci"
 
-echo "Date: $(date)\nBuild: $TRAVIS_BUILD_NUMBER"  > lastupdated
+git rm -rf ./javadoc
+cp -r ../doc/javadoc .
+
+printf "Date: $(date)\nBuild: $TRAVIS_BUILD_NUMBER"  > lastupdated
 git add -f .
 git commit -m "Javadoc for travis build: $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 
