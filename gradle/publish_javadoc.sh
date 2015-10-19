ff8c1f8f8280e85b2fddf571849e6ec49eeead42
Ho Yan Leung <hy.leung@gmail.com>
2015-10-18 19:09:45 -0700

    Ooops. Borked the javadoc

--- gradle/publish_javadoc.sh
+++ gradle/publish_javadoc.sh
@@ -4,13 +4,13 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Cloning gh-pages..."
     cd build
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hyleung/rx-testkit gh-pages 
-
+    
     cd gh-pages
     git config --local user.email "travis@travis-ci.org"
     git config --local user.name "travis ci"
 
-    git rm -rf ./javadoc
-    cp -r ../doc/javadoc .
+    git rm -rf javadoc
+    cp -r ../docs/javadoc . 
 
     printf "Date: $(date)\nBuild: $TRAVIS_BUILD_NUMBER"  > lastupdated
     git add -f .
