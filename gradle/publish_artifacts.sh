5b467b2c0b5ec533458261c1f04a31a95d0e1ca1
Ho Yan Leung <hy.leung@gmail.com>
2015-10-21 07:14:00 -0700

    Script fix

--- gradle/publish_artifacts.sh
+++ gradle/publish_artifacts.sh
@@ -3,7 +3,7 @@ cd $TRAVIS_BUILD_DIR
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then 
     echo "Publishing artifacts to Sonatype OSS..." 
-    ./gradlew upload -Psigning.keyId=$SIGNING_KEY -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=gradle/secring.gpg
+    ./gradlew upload -Psigning.keyId=$SIGNING_KEY_ID -Psigning.password=$SIGNING_PASSWORD -Psigning.secretKeyRingFile=gradle/secring.gpg
 else
     echo "Skipping artifact publish for $TRAVIS_BRANCH, $TRAVIS_BUILD_NUMBER"
 fi
