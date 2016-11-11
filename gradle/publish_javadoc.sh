49dfe0e58869cbe6a66f13fe0b18c2885c159057
Ho Yan Leung <hy.leung@gmail.com>
2016-11-11 07:57:57 -0800

    Publish javadoc to /docs on master

--- gradle/publish_javadoc.sh
+++ gradle/publish_javadoc.sh
@@ -2,27 +2,24 @@
 cd $TRAVIS_BUILD_DIR
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo "Cloning gh-pages..."
     cd build
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/hyleung/rx-testkit gh-pages 
-    
-    cd gh-pages
+
+    echo "Updating Javadoc..."
     git config --local user.email "travis@travis-ci.org"
     git config --local user.name "travis ci"
 
     rsync -r --delete ../docs/javadoc/ ./javadoc
 
-    printf "Date: $(date)\nBuild: $TRAVIS_BUILD_NUMBER"  > lastupdated
-    git add -u .
-    git add .
+    cd ../docs/javadoc
 
-    git commit -m "Javadoc for travis build: $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+    printf "Date: $(date)\nBuild: $TRAVIS_BUILD_NUMBER"  > lastupdated
+    git add .
+    git commit -m "Javadoc for travis build: $TRAVIS_BUILD_NUMBER auto-pushed to master"
 
     echo "Pushing changes to remote"
     git push
 
     cd ..
-    rm -rf gh-pages
 else
     echo "Skipping Javadoc publish for $TRAVIS_BRANCH, $TRAVIS_BUILD_NUMBER"
 fi
